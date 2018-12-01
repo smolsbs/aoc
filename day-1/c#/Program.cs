311140fb9caa4da698b8d4b479fb4d932401d4d4
Shino <shinobuaqt@waifu.club>
2018-12-01 07:03:19 +0000

    Day 1

---
+++ day-1/c#/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main()
+        {
+            string file = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()) + "\\input");
+            string[] aux = File.ReadAllText(file).Trim().Split('\n');
+            
+            Console.WriteLine(Part1(aux));
+            Console.WriteLine(Part2(aux));
+            
+        }
+
+        static int Part1(string[] inp){
+            int freq = 0;
+
+            foreach (string s in inp){
+                freq += int.Parse(s);
+            }
+
+            return freq;
+        }
+
+        static int Part2(string[] inp){
+            return 0;
+
+        }
+    }
+}
