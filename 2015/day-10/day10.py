a1cd1349ffcc724fcc2125c7bdb77a953c48dfc2
Sara <smol.sarabs@gmail.com>
2019-01-20 02:59:19 +0000

    merged from previous commit and added two new days

--- 2015/day-10/day10.py
+++ 2015/day-10/day10.py
@@ -1,15 +1,29 @@
-#!/usr/bin/env python3
-from itertools import groupby
-def main():
-    inp = "1113122113"
+#!/usr/bin/env python3\
 
-    for _ in range(50):
-        newinp = ""
-        for k, g in groupby(inp):
-            newinp += str(len(list(g))) + k
-        inp = newinp
-        if _ == 39:
-            print(len(inp))
+def look_say(inp):
+    if type(inp) != str:
+        inp = str(inp)
+    new_inp = []
+    ch = inp[0]
+    l = 1
+    for i in range(1, len(inp)):
+        if inp[i] == ch:
+            l += 1
+        if i != ch:
+            new_inp += (str(l) + inp[i])
+            l = 1
+        if i == len(inp):
+            new_inp += (str(l) + inp[i])
+    return ''.join(new_inp)
+
+
+def main():
+    inp = str(1113122113)
+    for _ in range(40):
+        print("i: {}\t len:{}".format(_, len(inp)))
+        inp = look_say(inp)
     print(len(inp))
+
 if __name__ == '__main__':
+    # print(look_say('1211'))
     main()
