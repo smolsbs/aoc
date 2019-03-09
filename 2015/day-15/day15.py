c0c76f71092e98eb4a741c631136595bf37c98a9
Sara <smol.sarabs@gmail.com>
2019-03-09 01:51:57 +0000

    soon:tm:

--- 2015/day-15/day15.py
+++ 2015/day-15/day15.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python
+from collections import defaultdict
+from re import findall
 
-import sys
+def listAllAmounts(total=100):
+    amts = []
+    for a in range(1, 101):
+        for b in range(1, 101-a):
+            for c in range(1, 101-(a+b)):
+                for d in range(1, 101-(a+b+c)):
+                    if a+b+c+d == total:
+                        amts.append({'Frosting':a, 'Candy':b, 'Butterscotch':c,'Sugar':d})
+    return amts
 
-from re import match
+def getScore(ing, n):
+    return sum([max(0, n*ing[i]) for i in ['c', 'd', 'f', 't']])
 
-
-def main(part2=None):
-    ingredients, caps, durs, flavs, texts, cals = [], [], [], [], [], []
-    solver = pywrapcp.Solver("Max")
-    regex = r'(\w+): capacity (\-?\d+), durability (\-?\d+), flavor (\-?\d+), texture (\-?\d+), calories (\-?\d+)'
+def main():
+    ings = defaultdict(dict)
     with open('input', 'r') as fp:
         for line in fp.read().strip().split('\n'):
-            ing, cap, dur, flav, text, cal = match(regex, line).groups()
-            _ingredient = solver.IntVar(0, 99, ing)
-            solver.Add(_ingredient >= 0)
-            ingredients.append(_ingredient)
-            caps.append(int(cap))
-            durs.append(int(dur))
-            flavs.append(int(flav))
-            texts.append(int(text))
-            cals.append(int(cal))
+            ing = line.split(':')[0]
+            c, d, f, t, cal = map(int, findall(r'\-?\d+', line))
+            ings[ing] = {'c':c, 'd':d, 'f':f, 't':t, 'cal':cal}
+    amnts = []
+    amounts = listAllAmounts()
+    for a in amounts:
+        score = 0
+        for k, v in a:
+            score += getScore(ings[k], v)
+        amnts.append(score)
+
+
+
+    print(sorted(amnts, reverse=True)[0:10])
+
+
 
 
 if __name__ == '__main__':
