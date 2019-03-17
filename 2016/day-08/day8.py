7091fc89e8b0568d961e4e76f31c966600237154
Sara <smol.sarabs@gmail.com>
2019-03-17 22:58:12 +0000

    day 8 rewrite

--- 2016/day-08/day8.py
+++ 2016/day-08/day8.py
@@ -1,44 +1,38 @@
 #!/usr/bin/python
-
 from re import findall
 import numpy as np
 
-def actOn(array, act):
-    if len(act) == 2:
-        array[0:act[1], 0:act[0]] = np.ones((act[1],act[0]), dtype='int')
-    elif len(act) == 3:
-        if act[0] == 'r':
-            array[act[1], :] = np.roll(array[act[1], :], act[2])
+def actOn(array, a):
+    if len(a) == 2:
+        array[0:a[0], 0:a[1]] = np.ones(a, dtype='int')
+    else:
+        if a[0] == 'r':
+            array[a[1], :] = np.roll(array[a[1], :], a[2])
         else:
-            array[:, act[1]] = np.roll(array[:, act[1]], act[2])
+            array[:, a[1]] = np.roll(array[:, a[1]], a[2])
 
     return array
 
-def printLine(line):
-    print(''.join(['#' if c == 1 else ' ' for c in list(line)]))
-
 def main():
-    actions = []
     screen = np.zeros((6,50), dtype='int')
     with open('input') as fp:
         for line in fp.read().strip().split('\n'):
             n1, n2 = map(int, findall(r'\d+',line))
             aux = line.split(' ')
             if len(aux) == 2:
-                actions.append( (n1, n2) )   # get rect size, n1=y, n2=x
+                screen = actOn(screen, (n2, n1))   # get rect size, n1=y, n2=x
             else:
                 if aux[1] == 'row':
-                    actions.append( ('r', n1, n2) )
+                    screen = actOn(screen, ('r', n1, n2))
                 else:
-                     actions.append( ('c', n1, n2) )
-    for action in actions:
-        screen = actOn(screen, action)
+                    screen = actOn(screen, ('r', n1, n2))
 
+    # part 1
     print(np.sum(screen))
 
+    # part 2
     for line in screen:
-
-        printLine(line)
+        print(''.join(['#' if c == 1 else ' ' for c in list(line)]))
 
 if __name__ == '__main__':
     main()
