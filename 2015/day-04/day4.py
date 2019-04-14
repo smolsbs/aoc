801e4789a8ed80d051fd2e36e4c44a2c80afea7b
Sara <smol.sarabs@gmail.com>
2019-04-14 01:23:48 +0100

    and added both parts to do together when running

--- 2015/day-04/day4.py
+++ 2015/day-04/day4.py
@@ -2,21 +2,26 @@
 #
 import hashlib
 
-def thing(secret):
+def thing(secret, part2=False):
     i = 10000
     while True:
         text = secret + str(i)
 
         hashed = hashlib.md5(bytes(text, 'utf-8')).hexdigest()
 
-        if hashed[:6] == '000000':
-            return text, hashed
+        if part2:
+            if hashed[:6] == '000000':
+                return text, hashed
+        else:
+            if hashed[:5] == '00000':
+                return text, hashed
         i +=1 
 
 def main():
     with open('input') as fp:
         secret = fp.read().strip()
     print(thing(secret))
+    print(thing(secret, True))
     
 
 
