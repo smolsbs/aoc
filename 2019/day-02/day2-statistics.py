eb6c35d015544a9eb0e57d9abc684333b05de51e
Sara <smol.sarabs@gmail.com>
2019-12-02 17:43:26 +0000

    clean up the code

--- 2019/day-02/day2-statistics.py
+++ 2019/day-02/day2-statistics.py
@@ -3,39 +3,33 @@ from random import choice
 import time
 import AoCUtils
 
-def Machine(intcode, value1, value2, verbose=False):
+def Machine(intcode, value1, value2):
     intcode = intcode.copy()
     intcode[1] = value1
     intcode[2] = value2
     ip = 0
     while True:
         opcode = intcode[ip]
-        if opcode == 99:
-            break
-        else:
-            valA_add = intcode[ip+1] 
-            valB_add = intcode[ip+2]
-            store_add = intcode[ip+3]
-
-            if opcode == 1:
-                intcode[store_add] = int(intcode[valA_add] + intcode[valB_add])
-            if opcode == 2:
-                intcode[store_add] = int(intcode[valA_add] * intcode[valB_add])
+        if opcode == 99: break
+        (pA, pB, rA) = intcode[ip+1 : ip+4]
+        if opcode == 1:
+            intcode[rA] = intcode[pA] + intcode[pB]
+        if opcode == 2:
+            intcode[rA] = intcode[pA] * intcode[pB]
 
         ip += 4
     return intcode[0]
 
 
 def main():
-    data = AoCUtils.loadInput('input')
-    data = list(map(int, data[0].split(',')))
+    data = list(map(int, AoCUtils.loadInput('input')[0].split(',')))
 
     # part 1 input
-    # print(Machine(data, 12, 2))
+    print(Machine(data, 12, 2))
     # part 2
     sdf = []
     sr = []
-    N_ATTEMPTS = 200
+    N_ATTEMPTS = 100
     print("Number of runs for each method:{}".format(N_ATTEMPTS))
     print("{}\nTesting double for".format('='*20))
     for _ in range(N_ATTEMPTS):
