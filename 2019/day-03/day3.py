8819c3b836ddac799849d8baf78a1b69d79e61b3
Sara <smol.sarabs@gmail.com>
2019-12-04 14:05:43 +0000

    re-wrote day3 and done day 4

--- 2019/day-03/day3.py
+++ 2019/day-03/day3.py
@@ -3,74 +3,51 @@
 import AoCUtils
 import numpy as np
 
-from pprint import pprint
-from scipy.spatial.distance import cdist
+from collections import defaultdict
 
-def lineIntersect(a1, a2, b1, b2):
-    # https://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect#565282
-    r = a1 - a2
-    s = b1 - b2
-    num = np.cross((b1-a1), r)  # ( q -p ) x r
-    denom = np.cross(r, s)      # r x s
-    if denom == 0:
-        # paralell
-        return -1
-    t = np.cross((b1-a1), s) / denom
-    u = np.cross((b1-a1),r) / denom
+def _getPoints(data):
+    moveset = {'D':(0,-1), 'U':(0,1), 'L':(-1,0), 'R':(1,0)}
+    seq = []
+    for line in data:
+        seq.append(line.split(','))
 
-    if num == 0 and denom == 0:
-        return -1
+    # store every single point where any of the wires pass and it's lenght
+    points = defaultdict(lambda: None)
 
-    if denom != 0 and np.abs(t) <= 1 and np.abs(u) <= 1:
-        return list(a1+t*r)
-    return -1
-
-def getPoints(seq):
-    (x, y) = 0.0, 0.0
-    steps = [ [np.array([0.,0.]), 0]]   # store all the points of the wires
-    i = 0                               # distance that the wire traveled (for part 2)
-    for act in seq:
-        (move, amount) = act[0].lower(), int(act[1:])
-
-        if move == 'u':
-            y += amount
-        elif move == 'd':
-            y -= amount
-        elif move == 'l':
-            x -= amount
-        elif move == 'r':
-            x += amount
-        i += amount
-        steps.append(( np.array([x, y]), i ))
-        
-    return steps
+    # iterate over each wire to path out both of them
+    for id in range(1, 3):
+        (x, y) = 0, 0
+        dist = 0
+        for act in seq[id-1]:
+            (move, amount) = moveset[act[0]], int(act[1:])
+            for j in range(amount):
+                x += move[0]
+                y += move[1]
+                dist += 1
+                if points[(x,y)] == None:
+                    points[(x,y)] = [set(), [], []]
+                points[(x,y)][0].add(id)
+                points[(x,y)][id].append(dist)
+    return points
 
 def manhattan(p1, p2):
-    return sum(abs(a-b) for a, b in zip(p1, p2))
+    return int(sum(abs(a-b) for a, b in zip(p1, p2)))
 
 def main():
-    # data = AoCUtils.loadInput('input')
-    data = AoCUtils.loadInput('sinput')
+    data = AoCUtils.loadInput('input')
 
-    crossed = []
-    wire1 = getPoints(data[0].split(','))
-    wire2 = getPoints(data[1].split(','))
+    # get every point
+    crossed = _getPoints(data)
 
-    # iterate over every pair of segments of wire and find if both intercept. If so,
-    # append the resulting point and wire distances to a list
+    # fitler out the points where only one wire passed through
+    same = {k:v[1:] for k,v in crossed.items() if len(v[0]) == 2}
+    same_p1 = list(same.keys()) # we only need the points themselves
+    same_p2 = {k:(v[0][0], v[1][0]) for k,v in same.items()} # get the first lenght for each wire
 
-    for i in range(0, len(wire1)-1, 1):
-        for j in range(0, len(wire2)-1, 1):
-            res = lineIntersect(wire1[i][0], wire1[i+1][0], wire2[j][0], wire2[j+1][0])
-            if res != -1:
-                crossed.append((res, wire1[i][1], wire2[j][1]))
-
-    p1 = sorted(crossed, key=lambda x: manhattan([0, 0], x[0]))
-    p2 = sorted(crossed, key=lambda x: x[1] + x[2])
-    print("part 1: {}".format( int(manhattan([0,0], p1[0][0])) ) )
-    print("part 2: {}".format(p2[0][1] + p2[0][2]))
-
-    
+    # sort for the cityblock distance and the sum of wire length, respectively 
+    p1 = manhattan([0,0],sorted(same_p1, key=lambda x: manhattan([0,0], x))[0])
+    p2 = sum(sorted(same_p2.values(), key=lambda x: sum(x))[0])
+    print("part1: {}\npart2: {}".format(p1,p2))
 
 
 if __name__ == '__main__':
