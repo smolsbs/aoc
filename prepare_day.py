499e7c9562d57ace1d2763a69a0f126b397cd714
sara <smol.sarabs@gmail.com>
2021-12-07 18:11:13 +0000

    slight change to prepare_day.py

--- prepare_day.py
+++ prepare_day.py
@@ -14,6 +14,12 @@ import aocUtils
 def main():
     data = aocUtils.loadInput('input')
 
+    p1 = None
+    p2 = None
+
+    print(f'part1: {p1}')
+    print(f'part2: {p2}')
+
 if __name__ == '__main__':
     main()"""
 
