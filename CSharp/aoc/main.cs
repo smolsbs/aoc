6ca9ab9ca46216338390d9808c34e20d63568707
Sara <smol.sarabs@gmail.com>
2022-04-06 18:10:19 +0100

    2021 day 2 done in C#

--- CSharp/aoc/main.cs
+++ CSharp/aoc/main.cs
@@ -1,35 +1,12 @@
-﻿using System;
-using System.Linq;
+﻿using Year2021;
 
-public class Day01{
+public class AdventOfCode{
+    
     public static void Main(){
-        int Counter = 0;
-        int PrevValue;
-        int[] UsrInput = File.ReadLines("/storage/git-repos/aoc/CSharp/aoc/day01.in")
-            .Select(r => int.Parse(r))
-            .ToArray();
+        var y_2021 = new y_2021();
 
-        // Part 1
-        PrevValue = UsrInput[0];
-        for(int i=1; i < UsrInput.Length; i++){
-            if (UsrInput[i] > PrevValue){
-                Counter++;
-            }
-            PrevValue = UsrInput[i];
-        }
-        Console.WriteLine($"Part 1: {Counter}");
+        // y_2021.Day_1("./year_2021/day01.in");
+        y_2021.Day_2("./year_2021/day02.in");
 
-        // Part 2
-        Counter = 0;
-        PrevValue = UsrInput[0..3].Sum();
-
-        for(int i=1; i < (UsrInput.Length - 2); i++){
-            int val = UsrInput[i..(i+3)].Sum();
-            if (val > PrevValue){
-                Counter++;
-            }
-            PrevValue = val;
-        }
-        Console.WriteLine($"Part 2: {Counter}");
     }
 }
