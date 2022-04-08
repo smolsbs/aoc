de225f329046a78b00e3968f523a10458c873d6a
Sara <smol.sarabs@gmail.com>
2022-04-08 19:26:29 +0100

    day 3 done in C#

--- CSharp/aoc/main.cs
+++ CSharp/aoc/main.cs
@@ -6,7 +6,8 @@ public class AdventOfCode{
         var y_2021 = new y_2021();
 
         // y_2021.Day_1("./year_2021/day01.in");
-        y_2021.Day_2("./year_2021/day02.in");
+        // y_2021.Day_2("./year_2021/day02.in");
+        y_2021.Day_3("./year_2021/day03.in");
 
     }
 }
