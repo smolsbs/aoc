d8625a0191d630995db63bde9631ea374d3e2852
Sara <smol.sarabs@gmail.com>
2022-07-19 20:51:11 +0100

    day 8 started on c#

--- CSharp/aoc/main.cs
+++ CSharp/aoc/main.cs
@@ -3,12 +3,11 @@
 public class AdventOfCode{
     
     public static void Main(){
-        var Y2021 = new Y2021();
 
-        // Y2021.Day1("./year_2021/day01.in");
-        // Y2021.Day2("./year_2021/day02.in");
-        // Y2021.Day3("./year_2021/day03.in");
-        Y2021.Day6("./year_2021/day06.in");
-
+        // y_2021.Day1("./year_2021/day01.in");
+        // y_2021.Day2("./year_2021/day02.in");
+        // y_2021.Day3("./year_2021/day03.in");
+        // y_2021.Day6("./year_2021/day06.in");
+        y_2021.Day8("./year_2021/day08.in");
     }
 }
