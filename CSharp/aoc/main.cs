308ea3640eb21b10a13d8bc8ca08d09b9748a2d7
Sara <smol.sarabs@gmail.com>
2022-07-26 02:13:50 +0000

    a

--- CSharp/aoc/main.cs
+++ CSharp/aoc/main.cs
@@ -7,12 +7,14 @@ public class AdventOfCode
     public static void Main()
     {
 
-        // y_2015.Day1("./year_2015/day01.in");
-        // y_2015.Day2("./year_2015/day02.in");
-        // y_2015.Day3("./year_2015/day03.in");
-        // y_2015.Day4("./year_2015/day04.in");
-        y_2015.Day5("./year_2015/day05.in");
-        // y_2015.Day6("./year_2015/day06.in");
+        // y_2015.Day1("./year_2015/day01.in"); // Done
+        // y_2015.Day2("./year_2015/day02.in"); // Done
+        // y_2015.Day3("./year_2015/day03.in"); // Done
+        // y_2015.Day4("./year_2015/day04.in"); // Done
+        // y_2015.Day5("./year_2015/day05.in"); // Done
+        // y_2015.Day6("./year_2015/day06.in"); // Done
+        y_2015.Day7("./year_2015/day07.in"); // Done
+
 
 
 
--- CSharp/aoc/year_2015/Year_2015.cs
+++ CSharp/aoc/year_2015/Year_2015.cs
@@ -14,7 +14,8 @@ public static class y_2015
 {
 
     private enum States { Off, On, Toggle };
-    private enum Niceness { Vowel, Double, Bad};
+    [Flags]
+    private enum Niceness { Def = 0, Vowel = 2, Double = 4, Bad = 8, Repeats = 16, Pair = 32 };
     public static void Day1(string Path)
     {
         string usrIn = File.ReadAllText(Path);
@@ -153,44 +154,70 @@ public static class y_2015
         Console.WriteLine($"Part 1: {p1}\nPart 2: {p2}");
     }
 
+
+    private static bool d5P1(string line)
+    {
+        var badStrings = new List<string> { "ab", "cd", "pq", "xy" };
+        var allVowels = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+        Niceness flags = default;
+        int vowels = 0;
+
+        for (int i = 0; i < line.Length; i++)
+        {
+
+            char c = line[i];
+
+            if (allVowels.Contains(c)) vowels++;
+
+            if (i < 1) continue;
+            char lc = line[i - 1];
+            if (badStrings.Contains($"{lc}{c}"))
+            {
+                flags |= Niceness.Bad;
+                break;
+            }
+            if (lc == c) flags |= Niceness.Double;
+        }
+        if (flags.HasFlag(Niceness.Bad)) return false;
+
+        if (vowels > 2) flags |= Niceness.Vowel;
+
+        return flags.HasFlag(Niceness.Vowel | Niceness.Double);
+    }
+
+    private static bool d5P2(string line)
+    {
+        Niceness flags = default;
+        for (int i = 0; i < line.Length; i++)
+        {
+            if (i < line.Length - 2)
+            {
+                string asdf = line[i..(i + 2)];
+                if (line[(i + 2)..].Contains(asdf)) flags |= Niceness.Pair;
+            }
+
+            if (i < 2) continue;
+            char llc = line[i - 2];
+            char lc = line[i - 1];
+            char c = line[i];
+
+            if (llc == c) flags |= Niceness.Repeats;
+        }
+
+        return flags.HasFlag(Niceness.Repeats | Niceness.Pair);
+    }
+
     public static void Day5(string Path)
     {
         string[] usrIn = File.ReadAllLines(Path).ToArray();
         int p1 = 0;
         int p2 = 0;
 
-        var badStrings = new List<string> { "ab", "cd", "pq", "xy" };
-        char lastChar;
-        char[] vowels;
-        Niceness nice;
-        bool ugly = false;
-
-
         foreach (string line in usrIn)
         {
-            lastChar = ' ';
-            vowels = new char[0];
-            ugly = false;
-            foreach (char c in line)
-            {
-                if (!vowels.Contains(c))
-                {
-                    vowels.Append(c);
-                }
-                if (badStrings.Any(s => s == @"{lastChar}{c}")){
-                    ugly = true;
-                    break;
-                }
-            }
-            if (ugly){
-                continue;
-            }
-            if (vowels.Length >= 3){
-                niceness++;
-            }
+            p1 += d5P1(line) == true ? 1 : 0;
+            p2 += d5P2(line) == true ? 1 : 0;
         }
-
-
         Console.WriteLine($"Part 1: {p1}\nPart 2: {p2}");
 
     }
@@ -267,6 +294,14 @@ public static class y_2015
 
         Console.WriteLine($"Part 1: {p1}\nPart 2: {p2}");
         // Your puzzle answer was 377891.
+
+    }
+
+
+    private enum Ops {And, Or, Lshift, Rshift};
+    public static void Day7(string Path)
+    {
+        string[] usrIn = File.ReadAllLines(Path).ToArray();
 
     }
 
