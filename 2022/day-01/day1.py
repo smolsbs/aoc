f90dddc5f3bcb1bcabd6beb17478a9c39a733c59
Shino <smol.sarabs@gmail.com>
2022-12-01 12:48:29 +0000

    2022 day 1 python

--- 2022/day-01/day1.py
+++ 2022/day-01/day1.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 
+from collections import deque
 import aocUtils
 
 def run(path):
-    data = aocUtils.loadInput(f"{path}/input")
+    cals = list()
+    fp = open(f"{path}/input", 'r')
 
-    p1 = None
-    p2 = None
+    c = 0
+    for line in fp.read().split('\n'):
+        if line != "":
+            c += int(line)
+        else:
+            cals.append(c)
+            c = 0
+    fp.close()
+    
+    cals = sorted(cals, reverse=True)[:3]
+    p1 = cals[0]
+    p2 = sum(cals)
 
     print(f'part1: {p1}')
     print(f'part2: {p2}')
+
