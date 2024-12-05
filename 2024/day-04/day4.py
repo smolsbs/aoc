3a03e38aa2e4e8900e0d0a1684f5bc6742a78e62
Shino <smol.sarabs@gmail.com>
2024-12-05 00:32:20 -0100

    formatting

--- 2024/day-04/day4.py
+++ 2024/day-04/day4.py
@@ -3,8 +3,7 @@
 import aocUtils
 from itertools import product
 
-COORDS = [v for v in product([-1,0,1], repeat=2) if v != (0,0)]
-
+COORDS = [v for v in product([-1, 0, 1], repeat=2) if v != (0, 0)]
 
 
 def make_grid(lines: list[str]) -> tuple[list[list[str]], int, int]:
@@ -13,29 +12,26 @@ def make_grid(lines: list[str]) -> tuple[list[list[str]], int, int]:
 
 
 def get_x(grid, pos_x, pos_y, max_x, max_y) -> int:
-
     w = ["" for _ in range(2)]
 
-    vert_bound = pos_y > 0 and pos_y < max_y-1
+    vert_bound = pos_y > 0 and pos_y < max_y - 1
     horiz_bound = pos_x > 0 and pos_x < max_x - 1
-    
+
     if vert_bound and horiz_bound:
-        w[0] = f"{grid[pos_y-1][pos_x-1]}A{grid[pos_y+1][pos_x+1]}"
-        w[1] = f"{grid[pos_y-1][pos_x+1]}A{grid[pos_y+1][pos_x-1]}"
+        w[0] = f"{grid[pos_y - 1][pos_x - 1]}A{grid[pos_y + 1][pos_x + 1]}"
+        w[1] = f"{grid[pos_y - 1][pos_x + 1]}A{grid[pos_y + 1][pos_x - 1]}"
 
     if w[0] in {"MAS", "SAM"} and w[1] in {"MAS", "SAM"}:
         return 1
     return 0
 
 
-
-
 def get_words(grid, pos_x, pos_y, max_x, max_y) -> int:
     words = ["X" for _ in range(len(COORDS))]
-    for i in range(1,4):
-        for idx,c in enumerate(COORDS):
-            x = pos_x + i*c[0]
-            y = pos_y + i*c[1]
+    for i in range(1, 4):
+        for idx, c in enumerate(COORDS):
+            x = pos_x + i * c[0]
+            y = pos_y + i * c[1]
 
             if (x >= 0 and x < max_x) and (y >= 0 and y < max_y):
                 words[idx] += grid[y][x]
@@ -48,21 +44,20 @@ def part1(grid, max_x, max_y):
 
     for yy in range(0, max_y):
         for xx in range(0, max_x):
-
-            if grid[yy][xx] != 'X':
+            if grid[yy][xx] != "X":
                 continue
 
             _sum += get_words(grid, xx, yy, max_x, max_y)
 
     return _sum
 
+
 def part2(grid, max_x, max_y):
     _sum = 0
 
     for yy in range(0, max_y):
         for xx in range(0, max_x):
-
-            if grid[yy][xx] != 'A':
+            if grid[yy][xx] != "A":
                 continue
 
             _sum += get_x(grid, xx, yy, max_x, max_y)
@@ -79,15 +74,19 @@ def run(path):
 
     return (p1, p2)
 
+
 # === Testing suite ===
 def get_sinput_path():
     from pathlib import Path
-    return Path(__file__).parent.absolute() 
+
+    return Path(__file__).parent.absolute()
+
 
 def test_p1():
     data = aocUtils.loadInput(f"{get_sinput_path()}/sinput")
     grid, m_x, m_y = make_grid(data)
     assert part1(grid, m_x, m_y) == 18
+
 
 def test_p2():
     data = aocUtils.loadInput(f"{get_sinput_path()}/sinput")
